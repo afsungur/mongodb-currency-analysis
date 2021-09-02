da8c3e3820c17268dd10586656828ca481602d19
Fuat Sungur <afsungur@gmail.com>
2021-09-02 17:28:20 +0400

    MongoDB Logo and Timeseries image has been added to the homepage

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import 'fomantic-ui-css/semantic.css';
 import Main from './Main'
 
+
+const styles = {
+  width: "200px"
+};
+
+const logoStyles = {width: "100px"}
+
 class App extends React.Component {
   render() {
     return (
@@ -17,6 +24,13 @@ class App extends React.Component {
           Top & Worst Performers
         </a>
         <div className="right menu">
+          <a className="item" href="https://cloud.mongodb.com" target="_blank">
+            <img src="https://webimages.mongodb.com/_com_assets/cms/MongoDB_Logo_FullColorBlack_RGB-4td3yuxzjs.png?auto=format%2Ccompress" style={styles}/>          
+            
+          </a>
+          <a className="item" href="https://docs.mongodb.com/manual/core/timeseries-collections/" target="_blank">
+            <img src="https://webimages.mongodb.com/_com_assets/cms/kqttj75kal7lsn48z-time_series_4@2x.png?auto=format%2Ccompress&ch=DPR" style={logoStyles}></img>
+          </a>
         </div>
       </div>
       <Main/>
