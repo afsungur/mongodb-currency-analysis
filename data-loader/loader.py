0d0db42353ece754b36d0eff760e03b020a128d6
Carlos Castro <carlos.castro@mongodb.com>
2021-09-22 10:51:03 +0100

    add new stock loader; set MongoDB version;

--- data-loader/loader.py
+++ data-loader/loader.py
@@ -1,4 +1,4 @@
-import crypto_binance_loader, crypto_coinbase_loader, sys
+import crypto_binance_loader, crypto_coinbase_loader, stock_finnhub_loader ,sys
 import logging, os
 
 exchange_data_source=os.environ['CURRENCY_LOADER_TYPE'] # BINANCE or COINBASE
@@ -8,5 +8,7 @@ if (exchange_data_source == "binance"):
     crypto_binance_loader.start_loader()
 elif (exchange_data_source == "coinbase"):
     crypto_coinbase_loader.start_loader()
+elif (exchange_data_source == "finnhub"):
+    stock_finnhub_loader.start_loader()
 
 
